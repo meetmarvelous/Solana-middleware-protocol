2de9b6576c4cde77cbbd2ce2ab4cae3eaeb056c4
sarthakNITT <sarthakkarodework@gmail.com>
2026-04-03 16:53:25 +0530

    optimise to add fee in tx

--- packages/fee-optimizer/src/optimize.ts
+++ packages/fee-optimizer/src/optimize.ts
@@ -1,5 +1,21 @@
-import type { SerializedTx } from "@repo/types/index";
+import type { DeserializedTx } from "@repo/types/index";
+import { ComputeBudgetProgram, Connection, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
 
-export function optimizeFee(tx: SerializedTx): SerializedTx {
-    return new Uint8Array();
+export async function optimizeFee(tx: DeserializedTx): Promise<DeserializedTx> {
+    const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+    const getFee = await connection.getRecentPrioritizationFees();
+    const fees = getFee
+        .map((e) => e.prioritizationFee)
+        .sort((a, b) => a - b)
+        .filter((e) => e > 0);
+    let baseFee: number = 0;
+    if (fees.length !== 0) {
+        const half = Math.floor(fees.length / 2);
+        fees.length % 2 ? baseFee = fees[half]! : baseFee = (fees[half - 1]! + fees[half]!) / 2;
+    }
+    const feeIx = ComputeBudgetProgram.setComputeUnitPrice({ microLamports: baseFee });
+    const message = TransactionMessage.decompile(tx.message);
+    message.instructions.unshift(feeIx);
+    const newTxWithFee = new VersionedTransaction(message.compileToV0Message());
+    return newTxWithFee;
 }
